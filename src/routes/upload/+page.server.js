66b44117d4aefa6ee018948528e54c01ecfe4b16
razzle <harry@razzle.cloud>
2023-06-08 14:44:20 -0500

    wire upload
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/routes/upload/+page.server.js
+++ src/routes/upload/+page.server.js
@@ -1,6 +1,8 @@
 import { fail } from "@sveltejs/kit";
 import fs from "fs";
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const actions = {
   default: async ({ request }) => {
     const formData = Object.fromEntries(await request.formData());
@@ -13,6 +15,10 @@ export const actions = {
     const { audioUpload } = formData;
 
     fs.writeFileSync(`static/${audioUpload.name}`, Buffer.from(await audioUpload.arrayBuffer()));
+
+    await sleep(6000);
+
+    fs.rmSync(`static/${audioUpload.name}`, { force: true });
 
     return {
       success: true,
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,12 +1,24 @@
 <script>
   const autorizedExtensions = [".mp3", ".m4a", ".wav", ".ogg"];
+  let uploading = false;
 </script>
 
 <svelte:head>
   <title>Upload</title>
 </svelte:head>
 
-<form method="post" use:enhance enctype="multipart/form-data" class="flex flex-col">
+<form
+  method="post"
+  use:enhance={() => {
+    uploading = true;
+    return async ({ update }) => {
+      await update();
+      uploading = false;
+    };
+  }}
+  enctype="multipart/form-data"
+  class="flex flex-col"
+>
   <input
     name="audioUpload"
     accept={autorizedExtensions.join(",")}
@@ -15,5 +27,5 @@
     class="file-input file-input-bordered file-input-lg w-full max-w-xs"
   />
 
-  <button type="submit" class="btn btn-primary mt-4">Upload</button>
+  <button disabled={uploading} type="submit" class="btn btn-primary mt-4">Upload</button>
 </form>

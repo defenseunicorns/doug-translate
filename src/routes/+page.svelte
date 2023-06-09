1525e19949e8746444d6f3073d20c9a5091d200a
razzle <harry@razzle.cloud>
2023-06-09 10:53:05 -0500

    style hacking
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   <title>Doug Translate</title>
 </svelte:head>
 
-<div class="hero min-h-screen bg-base-200">
+<div class="hero min-h-screen -mt-20">
   <div class="hero-content text-center">
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">Hello there</h1>

5c4a42e092cb996b0e4df749832ef3551bc6504f
razzle <harry@razzle.cloud>
2023-10-13 19:26:55 -0500

    metallb
    Signed-off-by: razzle <harry@razzle.cloud>

--- hack/cluster-up.sh
+++ hack/cluster-up.sh
@@ -6,4 +6,6 @@ k3d cluster create --api-port 6550 \
     -p '9080:80@loadbalancer' \
     -p '9443:443@loadbalancer' \
     --agents 2 \
-    --k3s-arg '--disable=traefik@server:*'
+    --k3s-arg '--disable=traefik@server:*' \
+    --k3s-arg '--disable=metrics-server@server:*' \
+    --k3s-arg '--disable=loadbalancer@server:*'

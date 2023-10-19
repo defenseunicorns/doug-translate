985d5f562f346afcdc119569939a6d9f74bcea77
razzle <harry@razzle.cloud>
2023-10-19 14:26:18 -0500

    rename
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/routes/upload/+error.svelte
+++ src/routes/upload/+error.svelte
@@ -6,7 +6,7 @@
   <div class="hero-content text-center">
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">{":|"}</h1>
-      <p class="py-6">Error: {$page.error.message}</p>
+      <p class="py-6">Error: {$page.error?.message ?? "Unknown"}</p>
       <a href="/upload"><button class="btn btn-info">Try Again</button></a>
     </div>
   </div>

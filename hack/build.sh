b5d314e07ce91d4fd81847af5877dae7faa12e81
razzle <harry@razzle.cloud>
2023-11-01 23:08:12 -0500

    remove istio and metallb deployments, superceded by dubbd
    Signed-off-by: razzle <harry@razzle.cloud>

--- hack/build.sh
+++ hack/build.sh
@@ -6,10 +6,6 @@ docker build . -t defenseunicorns/doug-translate:0.0.1
 
 mkdir -p pkgs
 
-zarf package create metallb -o pkgs --confirm
-
-zarf package create istio -o pkgs --confirm
-
 zarf package create -o pkgs --confirm
 
 # docker push defenseunicorns/doug-translate:latest

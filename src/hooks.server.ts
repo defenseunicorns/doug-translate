9f25007a1e9c233d9d28dbd91c058dcf0570f21a
razzle <harry@razzle.cloud>
2023-11-08 15:10:18 -0600

    feat: improve theme, add theme switcher, add help+contact page (#53)
    improve theme, add theme switcher, add help+contact page
    
    Signed-off-by: razzle <harry@razzle.cloud>

---
+++ src/hooks.server.ts
@@ -0,0 +1,15 @@
+const themes = ["winter", "dracula"];
+
+export const handle = async ({ event, resolve }) => {
+  const theme = event.cookies.get("theme");
+
+  if (!theme || !themes.includes(theme)) {
+    return await resolve(event);
+  }
+
+  return await resolve(event, {
+    transformPageChunk: ({ html }) => {
+      return html.replace('data-theme=""', `data-theme="${theme}"`);
+    },
+  });
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/routes/**/*.{svelte,js,ts}"],
+  content: ["./src/routes/**/*.{svelte,js,ts}", "./src/lib/components/**/*.{svelte,js,ts}"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["luxury"],
+    themes: ["dracula", "winter"],
   },
 };

af99926065d99da3138c434233032e03fe707b1c
razzle <harry@razzle.cloud>
2023-12-10 01:09:07 -0600

    use uds k3d instead of local nonsense
    Signed-off-by: razzle <harry@razzle.cloud>

--- hack/build.sh
+++ hack/build.sh
@@ -6,10 +6,6 @@ docker build . -t ghcr.io/defenseunicorns/doug-translate:0.1.0
 
 mkdir -p pkgs
 
-zarf package create metallb -o pkgs --confirm
-
-zarf package create istio -o pkgs --confirm
-
 zarf package create -o pkgs --confirm
 
 # docker push ghcr.io/defenseunicorns/doug-translate:latest

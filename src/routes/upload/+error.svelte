518ecf91cb6904a9523fd1ae45aa56ec37d8fedc
gharvey <gato.harvey@defenseunicorns.com>
2023-12-11 23:19:45 -0800

    Adds base path to error page

--- src/routes/upload/+error.svelte
+++ src/routes/upload/+error.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import {base} from "$app/paths";
 </script>
 
 <div class="hero min-h-screen -mt-20">
@@ -7,7 +8,7 @@
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">{":|"}</h1>
       <p class="py-6">Error: {$page.error?.message ?? "Unknown"}</p>
-      <a href="/upload"><button class="btn btn-info">Try Again</button></a>
+      <a href="{base}/upload"><button class="btn btn-info">Try Again</button></a>
     </div>
   </div>
 </div>
